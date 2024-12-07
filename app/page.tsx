6de8970e6b49c6dc85a293094a4f989992f16a14
Ceasar Attar <catta2@uic.edu>
2024-12-07 12:22:51 -0600

    website domain added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// app/page.tsx
-
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto px-6">
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-// next.config.js
+// // next.config.js
 
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true, // Enforces best practices
+
+// };
+
 const nextConfig = {
-  reactStrictMode: true, // Enforces best practices
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
